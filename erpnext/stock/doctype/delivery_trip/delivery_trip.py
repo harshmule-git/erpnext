0fa700f8fa12c7d01ae33ddcc57dd69c91ffff78
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2021-09-21 16:34:51 +0530

    chore: move delivery_trip.py code to erpnext from bloomstack_core (#1687)
    * chore: move delivery_trip.py code to erpnext from bloomstack_core
    
    * chore: move delivery_trip.py code to erpnext from bloomstack_core
    
    * chore: move delivery_trip.py code to erpnext from bloomstack_core

--- erpnext/stock/doctype/delivery_trip/delivery_trip.py
+++ erpnext/stock/doctype/delivery_trip/delivery_trip.py
@@ -13,6 +13,8 @@ from frappe.contacts.doctype.address.address import get_address_display
 from frappe.model.document import Document
 from frappe.utils import cint, flt, get_datetime, get_link_to_form, nowdate, today, unique
 from frappe.utils.password import get_decrypted_password
+from requests.utils import quote
+from erpnext.compliance.utils import make_integration_request
 
 
 class DeliveryTrip(Document):
@@ -28,14 +30,19 @@ class DeliveryTrip(Document):
 		self.validate_stop_addresses()
 		self.update_status()
 		self.update_package_total()
+		self.generate_directions_url()
+		self.link_invoice_against_trip()
 
 	def on_submit(self):
 		self.update_delivery_notes()
+		self.make_transfer_templates()
 
 	def on_update_after_submit(self):
 		self.update_delivery_note_status()
 		self.update_delivered_status_for_delivery_note()
 		self.validate_payment_due_date()
+		self.set_vehicle_last_odometer_value()
+
 	def before_submit(self):
 		self.update_status()
 
@@ -136,6 +143,63 @@ class DeliveryTrip(Document):
 
 		delivery_notes = [get_link_to_form("Delivery Note", note) for note in delivery_notes]
 		frappe.msgprint(_("Delivery Notes {0} updated".format(", ".join(delivery_notes))))
+
+	def generate_directions_url(self):
+		if not frappe.db.get_single_value("Google Settings", "enable"):
+			return
+
+		if not self.driver_address:
+			return
+
+		if self.delivery_stops:
+			route_list = self.form_route_list(optimize=False)
+
+			if not route_list:
+				return
+
+			route_list = route_list[0]
+
+			context = {
+				"key": get_decrypted_password("Google Settings", "Google Settings", "api_key"),
+				"origin": quote(route_list[0], safe=''),
+				"destination": quote(route_list[-1], safe=''),
+				"waypoints": quote('|'.join(route_list[1:-1]), safe='')
+			}
+
+			map_data = frappe.render_template("templates/includes/google_maps_embed.html", context)
+			self.map_embed = map_data
+
+	def link_invoice_against_trip(self):
+		for delivery_stop in self.delivery_stops:
+			if delivery_stop.delivery_note:
+				sales_invoice = frappe.get_all("Delivery Note Item",
+					filters={"docstatus": 1, "parent": delivery_stop.delivery_note},
+					fields=["against_sales_invoice"],
+					distinct=True)
+
+				if sales_invoice and len(sales_invoice) == 1:
+					delivery_stop.sales_invoice = sales_invoice[0].against_sales_invoice
+				# set sales invoice for cyecle sales order>>Delivery Note >>sales invoice>> then Delivery Trip from Delivery Note
+				if not sales_invoice[0].against_sales_invoice:
+					sales_invoice = frappe.get_all("Sales Invoice Item",
+						filters={"docstatus": 1, "delivery_note": delivery_stop.delivery_note},
+						fields=["distinct(parent)"])
+					if len(sales_invoice)==1:
+						delivery_stop.sales_invoice = sales_invoice[0].parent
+
+	def make_transfer_templates(self):
+		for stop in self.delivery_stops:
+			if not stop.delivery_note:
+				continue
+
+			for item in frappe.get_doc("Delivery Note", stop.delivery_note).items:
+				if frappe.db.get_value("Item", item.item_code, "is_compliance_item"):
+					make_integration_request("Delivery Note", stop.delivery_note, "Transfer")
+					break
+
+	def set_vehicle_last_odometer_value(self):
+		if self.actual_distance_travelled:
+			frappe.db.set_value('Vehicle', self.vehicle, 'last_odometer', self.odometer_end_value)
 
 	def update_delivered_status_for_delivery_note(self):
 		for stop in self.delivery_stops:
@@ -640,3 +704,8 @@ def update_delivery_trip_status(payment_amount, sales_invoice):
 				frappe.db.set_value("Delivery Stop", stop.name, "visited", 1)
 				if stop.delivery_note:
 					frappe.db.set_value("Delivery Note", stop.delivery_note, "status", "Completed")
+
+@frappe.whitelist()
+def get_address_display_for_trip(address):
+	address_details = frappe.db.get_value("Address", address, "*", as_dict=True)
+	return frappe.render_template("erpnext/regional/united_states/address_template.html", address_details)
